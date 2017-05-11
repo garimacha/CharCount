1c46ed73a5a0bdcf4a475b037c102f9a80f92384
garich01 <garich01@noa.nintendo.com>
2017-05-11 12:05:46 -0700

    refactoring new code

---
+++ ConsoleApplication/DuplicateChar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApplication
+{
+    public class DuplicateChar
+    {
+
+        public static void duplicateChar(String str)
+        {
+            //WAP to print the duplicate char from the string
+
+            Hashtable hs= new Hashtable();
+            Char[] ch = str.ToCharArray();
+            char ch1 = ' ';
+
+            for (int i = 0; i <= ch.Length - 1; i++)
+            {
+
+                //if there is no value then put 1
+                if (hs[ch[i]] == null)
+                {
+                    hs[ch[i]] = 1;
+
+                }
+                else
+                {
+                    int num;
+                    num = Convert.ToInt32(hs[ch[i]]);
+                    hs[ch[i]] = num+1;
+                }
+            }
+
+            for (int j = 0; j <= ch.Length - 1; j++)
+            {
+                if (Convert.ToInt32(hs[ch[j]]) > 1)
+                {
+                    Console.Write(ch[j].ToString());
+                }
+            }
+
+        }
+
+    }
+}
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+
 
 namespace ConsoleApplication
 {
@@ -8,35 +7,10 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            //wap to print the number of times the char is present in the string
+            DuplicateChar.duplicateChar("hello");
 
-            var str = "hello";
-            Hashtable hs=new Hashtable();
 
-            // ReSharper disable once SuggestVarOrType_Elsewhere
-            char[] ch = str.ToCharArray();
 
-            for (int i = 0; i <= ch.Length-1; i++)
-            {
-                var ch1 = ch[i];
-
-                if (hs[ch1] == null)
-                {
-                    hs[ch1] = 1;
-                }
-                else
-                {
-                    int num;
-                    num = (int) hs[ch1];
-                    num = num + 1;
-
-                }
-            }
-
-            foreach (DictionaryEntry ds in hs)
-            {
-                Console.WriteLine(ds.Key.ToString() + ds.Value.ToString());
-            }
         }
     }
 }
